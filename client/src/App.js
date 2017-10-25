cf3e9d48ab1b36ff9a7382c0844dc60835287477
Josh Leichtung <joshleichtung@gmail.com>
2017-10-24 17:30:27 -0700

    Initial map react setup

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+// import from "react-mapbox-gl/css";
+
+const Map = ReactMapboxGl({
+  accessToken: "pk.eyJ1Ijoia2V2aW5jYWk3OSIsImEiOiJjajk2YXBqMHUwMjd6MnpvbHU3a3FiODE4In0.Akrpxhy1oIxzIQ34EB1adg"
+});
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Parking Predixion</h1>
+        <Map
+          style="mapbox://styles/mapbox/streets-v9"
+          containerStyle={{
+            height: "100vh",
+            width: "100vw"
+          }}>
+          <Layer
+            type="symbol"
+            id="marker"
+            layout={{ "icon-image": "marker-15" }}>
+            <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
+          </Layer>
+      </Map>
+      </div>
+    );
+  }
+}
+
+export default App;
