bf7308fdd97fc800fae633be1c8c097454512ae4
Josh Leichtung <joshleichtung@gmail.com>
2017-10-26 00:01:51 -0700

    Add parking suggestions to map

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import ReactMapboxGl, { Layer, Feature, Marker, ScaleControl } from "react-mapbox-gl";
+import ReactMapboxGl, { Layer, Popup, Feature, Marker, ScaleControl } from "react-mapbox-gl";
 import axios from 'axios'
 
 const Map = ReactMapboxGl({
@@ -14,25 +14,27 @@ class App extends Component {
       coordinates: [-117.157122, 32.712854],
       textInput: '',
       parking: [],
-      suggestions: []
+      suggestions: [],
+      showSuggestions: false
     }
     this.updateTextInput = this.updateTextInput.bind(this)
     this.submitTextInput = this.submitTextInput.bind(this)
   }
 
   componentDidMount() {
-    // navigator.geolocation.getCurrentPosition((position) => {
-    //   this.setState({coordinates: [position.coords.longitude, position.coords.latitude]});
-    // });
-    console.log(`https://fiery-doves-server-v2.run.aws-usw02-pr.ice.predix.io?ts=${Number(new Date)}&long=${this.state.coordinates[0]}&lat=${this.state.coordinates[1]}`)
-    axios.get(`https://fiery-doves-server-v2.run.aws-usw02-pr.ice.predix.io?ts=${Number(new Date)}&long=${this.state.coordinates[0]}&lat=${this.state.coordinates[1]}`)
+    axios.get(`https://fiery-doves-server-v2.run.aws-usw02-pr.ice.predix.io?ts=${Math.floor(Number(new Date()) / 1000)}&long=${this.state.coordinates[0]}&lat=${this.state.coordinates[1]}`)
       .then(({ data: {zones, suggestions}}) => {
         console.log("zones: ", zones, "suggestions: ", suggestions)
         this.setState({
           parking: zones,
           suggestions: suggestions
         });
-      });
+      })
+      .catch(error => console.log("Ali Server error: ", error))
+  }
+
+  componentDidUpdate() {
+    console.log("update")
   }
 
   updateTextInput(e) {
@@ -61,12 +63,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Parking PrediXion</h1>
+        <h1><span className="big-p">P</span>arking <span className="big-p">P</span>redi<span className="big-p">X</span>ion</h1>
         <Map
           center={this.state.coordinates}
           style="mapbox://styles/mapbox/streets-v9"
-	        onStyleLoad={this._onStyleLoad.bind(this)}
-	        zoom={[16.5]}
+          onStyleLoad={this._onStyleLoad.bind(this)}
+          zoom={[16.5]}
           containerStyle={{
             height: "100vh",
             width: "100vw"
@@ -77,6 +79,14 @@ class App extends Component {
           >
             <img src="pin.png" style={{height: "45px", width: "45px"}}/>
           </Marker>
+          {this.state.suggestions.map(({msg, long, lat}, idx) => (
+            <Popup
+              coordinates={[long, lat]}
+              key={idx}
+            >
+              {msg}
+            </Popup>
+          ))}
         </Map>
         <input type="text"
           onChange={this.updateTextInput}
@@ -99,11 +109,33 @@ class App extends Component {
             top: "80px",
             left: "20px",
             backgroundColor: "rgba(255, 255, 255, .7)",
-            padding: "1.5em"
+            textAlign: "left"
 
           }}
         >
-          This stuff is happending
+          <h2>
+            <a 
+              href="#"
+              onClick={() => this.setState({
+                showSuggestions: !this.state.showSuggestions
+              })}
+            >
+              Parking Suggestions
+            </a>
+          </h2>
+          <ul style={{padding: "0"}} className={this.state.showSuggestions ? "" : "hidden"}>
+          {this.state.suggestions.map(({msg, long, lat}, idx) => (
+            <li style={{listStyleType: "none"}} key={idx}>
+            <a  href="#"
+                onClick={() => this.setState({
+              coordinates: [long, lat]
+            })}
+          >
+            {msg.trim()}
+          </a>
+        </li>
+          ))}
+        </ul>
         </div>
       </div>
     );
